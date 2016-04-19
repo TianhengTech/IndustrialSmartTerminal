0c92de8854d23d21e746a42bcb509b82249a280a
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-19 16:08:41 +0800

    数据处理类

---
+++ BaseClass/BaseClass/TerminalUltility/DataProcessBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClass.TerminalUltility
+{
+    class DataProcessBase
+    {
+        public DataProcessBase()
+        {
+
+        }
+        protected object HexToByte(string hex)
+        {
+            return true;
+        }
+        protected object ByteToSingle(byte[] bytes)
+        {
+            return true;
+        }
+        protected object ByteToString(byte[] bytes)
+        {
+            return true;
+        }
+
+    }
+}
