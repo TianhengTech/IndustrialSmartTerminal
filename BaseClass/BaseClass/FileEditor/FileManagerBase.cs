f27ea3e036dc8fcbbd3093aac4713f6407462861
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-19 13:14:28 +0800

    基类

---
+++ BaseClass/BaseClass/FileEditor/FileManagerBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClass.FileEditor
+{
+    class FileManagerBase
+    {
+        public FileManagerBase()
+        {
+
+        }
+        protected object Open(string filepath)
+        {
+            return true;
+        }
+        protected object Write(string filepath)
+        {
+            return true;
+        }
+        protected object Read(string filepath)
+        {
+            return true;
+        }
+        protected object Close(string filepath)
+        {
+            return true;
+        }
+        protected object IsOpen(string filepath)
+        {
+            return true;
+        }
+        protected object MakeDir(string Dirpath)
+        {
+            return true;
+        }
+        protected object MakeFile(string filepath)
+        {
+            return true;
+        }
+
+
+
+
+    }
+}
