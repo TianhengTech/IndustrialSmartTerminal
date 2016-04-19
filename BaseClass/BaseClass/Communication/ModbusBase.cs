b17c856360c50c208efe206f832cdf8799bf1207
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-19 16:09:10 +0800

    modbus通信类

---
+++ BaseClass/BaseClass/Communication/ModbusBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClass.Communication
+{
+    class ModbusBase
+    {
+        String Connectiontype;
+        public ModbusBase()
+        {
+
+        }
+
+        protected object TCPConnect(string ip,int port)
+        {
+            return true;
+        }
+        protected object RTUConnect(string port,params object[] list)
+        {
+            return true;
+        }
+        protected object GetPortStatus(string port)
+        {
+            return true;
+        }
+        protected object Disconnect()
+        {
+            return true;
+        }
+        protected object IsConnected()
+        {
+            return true;
+        }
+        protected object ReadRegister(int slaveaddress,string registertype,int start,int amount,byte[] ReadData)
+        {
+            return true;
+        }
+        protected object WriteRegister(int slaveaddress, string registertype,int start,byte[] WriteData)
+        {
+            return true;
+        }
+
+    }
+}
