b25768e6ccb5e950da237b19a989997f06ad7fba
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-19 16:43:02 +0800

    串口通信类

---
+++ BaseClass/BaseClass/Communication/SerialBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClass.Communication
+{
+    class SerialBase
+    {
+        bool _keepreading;
+        SerialPort serialport;
+        public SerialBase(params object[] parameters)
+        {
+        }
+        protected object OpenPort()
+        {
+            return true;
+        }
+        protected object ClosePort()
+        {
+            return true;
+        }
+        protected object IsOpen
+        {
+            get
+            {
+                return serialport.IsOpen;
+            }
+        }
+        protected int SetReadTimeOut
+        {
+            set
+            {
+                serialport.ReadTimeout = value;
+            }
+        }
+        protected int SetReadWriteOut
+        {
+            set
+            {
+                serialport.WriteTimeout = value;
+            }
+        }
+
+        protected object SendCmd(byte[] cmd)
+        {
+            return true;
+        }
+        protected object ReadPort(byte[] readbuffer)
+        {
+            return true;
+        }
+        
+
+    }
+}
