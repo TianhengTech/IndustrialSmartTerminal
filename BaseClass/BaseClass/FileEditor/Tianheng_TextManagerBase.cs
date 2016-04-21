dac0d4c35dc57d1bd0476c82d27d7fb5cb07b2fc
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-21 10:55:53 +0800

    文件操作基类

---
+++ BaseClass/BaseClass/FileEditor/Tianheng_TextManagerBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClass.FileEditor
+{
+    class Tianheng_TextManagerBase
+    {
+        public Tianheng_TextManagerBase()
+        {
+
+        }
+
+        protected virtual  object Open(string filepath)
+        {
+            return true;
+        }
+        protected virtual  object Write(string filepath)
+        {
+            return true;
+        }
+        protected virtual  object ReadIniAllKeys(string filepath)
+        {
+            return true;
+        }
+        protected virtual  object Close(string filepath)
+        {
+            return true;
+        }
+        protected virtual  object IsOpen(string filepath)
+        {
+            return true;
+        }
+        protected virtual  object MakeDir(string Dirpath)
+        {
+            return true;
+        }
+        protected virtual  object MakeFile(string filepath)
+        {
+            return true;
+        }
+        public virtual bool DeleteDir(string filepath)
+        {
+            return true;
+        }
+        public virtual bool DeleteFile(string filepath)
+        {
+            return true;
+        }
+        protected virtual  object Exists(string filepath)
+        {
+            return true;
+        }
+
+
+
+
+    }
+}
