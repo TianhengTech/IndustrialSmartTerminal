83891d34243886c0d330ec37d4aed4784979fd85
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-21 11:31:59 +0800

    更改读写函数参数类型

--- BaseClass/BaseClass/Communication/ModbusIOBase.cs
+++ BaseClass/BaseClass/Communication/ModbusIOBase.cs
@@ -1,55 +1,65 @@
-﻿using System;
+﻿using Modbus.Device;
+using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BaseClass.Communication
 {
-    class ModbusIOBase
+    abstract class ModbusIOBase
     {
         String Connectiontype;
         public ModbusIOBase()
         {
 
         }
-
-        protected object TCPConnect(string ip,int port)
+        protected virtual void ModbusRtuInit(string portName, int baudrate = 115200, Parity parity = Parity.None)
         {
-            return true;
+            return;
         }
-        protected object RTUConnect(string port,params object[] list)
+
+        protected virtual void TcpConnect(string ipAddress,int tcpPort)
         {
-            return true;
+            return;
+        }
+        public virtual void TcpReconnect()
+        {
+            return;
+        }
+        public abstract IModbusMaster CreateRtuMaster(string portName, int baudrate = 115200, int DataBits = 8, StopBits stopbits = StopBits.One, Parity parity = Parity.None);
+        public abstract IModbusMaster CreateTcpMaster(string ip, int tcpport);
+        public virtual int retries
+        {
+            set;
+            get;
         }
         protected object GetPortStatus(string port)
         {
             return true;
         }
-        protected object Disconnect()
+        public virtual void Destory()
         {
-            return true;
+            return;
         }
         protected object IsConnected
         {
             get { return true; }
         }
-        protected object ReadRegister(int slaveaddress,string registertype,int start,int amount,byte[] ReadData)
-        {
-            return true;
-        }
-        protected object WriteRegister(int slaveaddress, string registertype,int start,byte[] WriteData)
-        {
-            return true;
-        }
-        protected int SetReadTimeOut
+        public abstract ushort[] ReadRegister(byte slaveaddress, string registertype, int start, int amount);
+        public abstract ushort[] WriteRegister(byte slaveaddress, string registertype, int start, byte[] WriteData);
+        public override bool[] ReadInOut(byte slaveaddress, int type, ushort start, ushort amount);
+        public override bool[] WriteInOut(byte slaveaddress, int type, ushort start, ushort amount, bool[] WriteData);
+
+        public virtual int SetReadTimeOut
         {
             set
             {
                 
             }
         }
-        protected int SetReadWriteOut
+        public virtual int SetReadWriteOut
         {
             set
             {
