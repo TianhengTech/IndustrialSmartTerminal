5f71150653059f6f05bb772d9d282efa81e53375
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-24 11:56:22 +0800

    修改代码风格

--- BaseClass/BaseClass/Communication/ProfinetCommunicationBase.cs
+++ BaseClass/BaseClass/Communication/ProfinetCommunicationBase.cs
@@ -1,91 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Snap7;
 
 namespace BaseClass.Communication
 {
-    public abstract class ProfinetCommunicationBase
+    internal class ProfinetCommunicationBase
     {
-        public ProfinetCommunicationBase()
+        protected ProfinetCommunicationBase()
         {
-
         }
-        public virtual object SetParameters(string Address,UInt16 LocalTSAP,UInt16 RemoteTSAP)
+
+        public virtual object SetParameters(string address, ushort localTsap, ushort remoteTsap)
         {
             return true;
         }
-        public virtual int ConnectTo(string Address,int Rack,int Slot)
+
+        public virtual int ConnectTo(string address, int rack, int slot)
         {
             return 0;
         }
-        public virtual int ConnectTo_200(string Address, UInt16 LocalTSAP, UInt16 RemoteTSAP)
+
+        public virtual int ConnectTo_200(string address, ushort localTsap, ushort remoteTsap)
         {
             return 0;
         }
+
         public virtual int Connect()
         {
             return 0;
         }
+
         public virtual int Disconnect()
         {
             return 0;
         }
+
         public virtual bool IsConneted()
         {
             return true;
         }
+
         public virtual int PlcStop()
         {
             return 0;
         }
+
         public virtual int PlcHotStart()
         {
             return 0;
         }
+
         public virtual int PlcColdStart()
         {
             return 0;
         }
-        public virtual object Destory(object Client)
+
+        public virtual object Destory(object client)
         {
-            Client = null;
+            client = null;
             return true;
         }
-        public virtual int Read(int Area,int start,int Amount,int WordLen,byte[] buffer)
+
+        public virtual int Read(int area, int start, int amount, int wordLen, byte[] buffer)
         {
             return 0;
         }
-        public virtual int ReadDB(int Area,int DBnumber, int start, int Amount, int WordLen, byte[] buffer)
+
+        public virtual int ReadDb(int area, int dBnumber, int start, int amount, int wordLen, byte[] buffer)
         {
             return 0;
         }
-        public virtual int Write(int Area, int start, int Amount, int WordLen, byte[] buffer)
+
+        public virtual int Write(int area, int start, int amount, int wordLen, byte[] buffer)
         {
             return 0;
         }
-        public virtual int WriteDB(int Area,int DBnmuber, int start, int Amount, int WordLen, byte[] buffer)
+
+        public virtual int WriteDb(int area, int dBnmuber, int start, int amount, int wordLen, byte[] buffer)
         {
             return 0;
         }
-        public virtual string PLCstatus()
+
+        public virtual string PlCstatus()
         {
             return "";
         }
+
         public virtual DateTime GetPlctime()
         {
             return DateTime.Today;
         }
+
         public virtual int SetPlctime(DateTime time)
         {
             return 0;
         }
-        public virtual int ReadMultiVars(Snap7.S7Client.S7DataItem[] itemlist,int itemcount)
+
+        public virtual int ReadMultiVars(S7Client.S7DataItem[] itemlist, int itemcount)
         {
             return 0;
         }
-        public virtual int WriteMultiVars(Snap7.S7Client.S7DataItem[] itemlist,int itemcount)
+
+        public virtual int WriteMultiVars(S7Client.S7DataItem[] itemlist, int itemcount)
         {
             return 0;
         }
