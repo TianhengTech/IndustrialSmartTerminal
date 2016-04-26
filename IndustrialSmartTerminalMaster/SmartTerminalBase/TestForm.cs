dd0624f060eb6eb28e74eddea8eb3d1598f5897e
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-26 13:51:02 +0800

    文件名提交

---
+++ IndustrialSmartTerminalMaster/SmartTerminalBase/TestForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BaseClass.FileEditor;
+
+namespace BaseClass
+{
+    public partial class TestForm : UDiskMonitor
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+           // UDiskMonitor monitor = new UDiskMonitor();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
