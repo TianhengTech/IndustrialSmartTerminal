b8cb1d39be35187b18ab2bc20aca9c70b5536f43
yufei.zhang <yufei.zhang@tianheng-uestc.com>
2016-04-29 14:36:41 +0800

    update

---
+++ IndustrialSmartTerminalMaster/SmartTerminalBase/DataBase/NhibernateGroup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+namespace SmartTerminalBase.DataBase
+{
+    class historydata
+    {
+        public virtual int idhistorydata {set;get; }
+        public virtual string json_string { set; get; }
+        public virtual DateTime storetime { set; get; }
+    }
+
+    class historydataMapping:ClassMap<historydata>
+    {
+        public historydataMapping()
+        {
+            Table("historydata");
+            Id<int>("idhistorydata").GeneratedBy.Identity();
+            Map(m => m.json_string).Nullable();
+            Map(m => m.storetime).Nullable();
+
+        }
+    }
+}
